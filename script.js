d010ed25b6da92e998326c6010765bf0b28e3f45
tauheed0110 <tauheed0110@gmail.com>
2024-11-05 16:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+const radius = document.getElementById("radius");
+const volume = document.getElementById("volume");
+const submit = document.getElementById("submit");
+
+
+submit.addEventListener('click', ()=>{
+    volume_sphere();
+})
+
 function volume_sphere() {
-    //Write your code here
-  
+    // Write your code here
+    if(isNaN(radius)){
+        volume.value = "NaN";
+        return;
+    }
+    const radius = parseInt(radius);
+    const v = ((4/3)*Math.PI*radius*radius*radius);
+    volume.value = v.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
